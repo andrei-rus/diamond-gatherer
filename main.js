eb6fc9ce9e0e0ae3418ebfc9b6aecbd169666f31
Andrei Rus <rus.andrey93@outlook.com>
2020-11-11 19:38:10 +0200

    Curs 1 - Introducere html, css, javascript

---
+++ main.js
@@ -0,0 +1,17 @@
+var name = "Paul"; // string
+var ammount = 23; // numeric
+var knowJavascript = true; //boolean
+var fruits = ["apple", "lemon", "orange"]; // Array
+
+function add() {
+    return 2 + 3;
+}
+
+var person = {
+    height: 2,
+    canWalk : function() {
+        console.log("Yes, I can walk");
+    },
+    name: "julia"
+}
+
