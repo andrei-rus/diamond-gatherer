ab1ae270f2ffa5dd579fe5394608d7ca4c84be02
Andrei Rus <rus.andrey93@outlook.com>
2020-11-18 19:59:24 +0200

    Curs 2 - Teorie + Animatie player

--- main.js
+++ main.js
@@ -1,17 +1,62 @@
-var name = "Paul"; // string
-var ammount = 23; // numeric
-var knowJavascript = true; //boolean
-var fruits = ["apple", "lemon", "orange"]; // Array
+import { Animal } from './animal.js';
 
-function add() {
-    return 2 + 3;
+const canvas =  document.getElementById("canvasId");
+/** @type {CanvasRenderingContext2D} */
+const context = canvas.getContext('2d');
+
+const myPet = new Animal('Rocky');
+myPet.canEat();
+
+// context.fillStyle = "red";
+// context.fillRect(280, 20, 40, 20);
+
+const geoerge = new Image();
+geoerge.src = 'assets/george.png'
+const GEORGE_WIDTH = 40;
+const GEORGE_HEIGHT = 45;
+let georgeX = 100;
+let georgeY = 100;
+geoerge.onload = () => {
+    context.drawImage(geoerge, 0 * GEORGE_WIDTH, 0 * GEORGE_HEIGHT, GEORGE_WIDTH, GEORGE_HEIGHT, 100, 100, GEORGE_WIDTH, GEORGE_HEIGHT)
 }
 
-var person = {
-    height: 2,
-    canWalk : function() {
-        console.log("Yes, I can walk");
-    },
-    name: "julia"
+const mario = new Image();
+mario.src = 'assets/mario.png'
+const MARIO_WIDTH = 32;
+const MARIO_HEIGHT = 39;
+mario.onload = () => {
+    context.drawImage(mario, 0 * MARIO_WIDTH, 0 * MARIO_HEIGHT, MARIO_WIDTH, MARIO_HEIGHT, 0, 0, MARIO_WIDTH, MARIO_HEIGHT)
 }
+
+const button = document.getElementById("myButton");
+button.addEventListener("click", function() {
+    console.log(this);
+    context.fillStyle = "green";
+    context.fillRect(480, 20, 40, 20);
+});
+
+document.addEventListener("keydown", function(event) {
+    context.clearRect(0, 0, 600, 400);
+    switch(event.key) {
+        case 'ArrowUp': {
+            georgeY -= 10;
+            break;
+        }
+        case 'ArrowDown': {
+            georgeY += 10;
+            break;
+        }
+        case 'ArrowLeft': {
+            georgeX -= 10;
+            break;
+        }
+        case 'ArrowRight': {
+            georgeX += 10;
+            break;
+        }
+    }
+
+    context.drawImage(mario, 0 * MARIO_WIDTH, 0 * MARIO_HEIGHT, MARIO_WIDTH, MARIO_HEIGHT, 0, 0, MARIO_WIDTH, MARIO_HEIGHT);
+    context.drawImage(geoerge, 0 * GEORGE_WIDTH, 0 * GEORGE_HEIGHT, GEORGE_WIDTH, GEORGE_HEIGHT, georgeX, georgeY, GEORGE_WIDTH, GEORGE_HEIGHT)
+});
 
